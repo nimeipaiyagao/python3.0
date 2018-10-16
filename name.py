0b497e1251ce97e5fa9f8c4a6bbb4c97af83c306
nimeipaiyagao <547429755@qq.com>
2018-10-16 23:35:14 +0800

    qwer

---
+++ name.py
@@ -0,0 +1,102 @@
+'''name = 'ada lovelace'
+print(name.title())
+
+name = 'Ada Lovelace'
+print(name.upper()) 
+print(name.lower())
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name + '' + last_name
+print(full_name)
+
+first_name = 'ada'
+last_name = 'lovelace'
+full_name = first_name + '' + last_name
+print('hello,'+full_name.title()+'!')
+
+
+first_name = 'yao'
+last_name = 'yuan'
+full_name = first_name + '' + last_name
+message = 'Hello,' + full_name.title() + '!'
+print(message)
+
+
+
+print('\tpython')
+print('\npython')
+print('language:\n\t123\n\t123\n\t123\n\t123')
+
+name = 'alex'
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+name = 'yaoyuan'
+message = 'would you like to learn some python today'
+fullmessage = 'hello '+name+','+message
+print(fullmessage)
+
+name = 'Alert Einstein'
+saying = 'A person who never made a mistake never tried anything new.'
+print(name,"once said,\"",saying,"\"")
+
+
+name = 'Alert Einstein'
+saying = 'A person who never made a mistake never tried anything new.'
+message = name+" once said,\" "+saying+"\""
+print(message)
+
+
+
+name = 'xin'
+message = 'hello '+name+',would you like to learn some python today'
+print(message)
+
+
+message = 'Albert Einstein once said,"A person who  made a mistake never tried anything new"'
+print(message)
+
+famous_person = 'Albert Einstein'
+message = famous_person+' once said,"A person who  made a mistake never tried anything new"'
+print(message)
+
+
+print('\tpython')
+print('\npython')
+print('language:\n\t123\n\t123\n\t123\n\t123')
+
+
+
+print(5+3)
+print(2*4)
+print(9-1)
+print(int(8/1))
+
+
+number = 888
+love = "I LOVE U "+str(number)
+print(love)'''
+
+
+
+
+name = 'eric'
+message = 'hello '+name+' ,would you like to learn some Python today'
+print(message)
+ 
+
+age = 18 
+message = 'happy ' + str(age) + 'rd birthday'
+print (message)  
+
+
+
+
+
+
+
+
+
+
